a1138393a5c4ad32598539a2c7b55aa4466ef705
WalkerKenneth <kennethesteban@hotmail.com>
2024-10-29 12:30:06 -0600

    Event added

--- foundry-smart-contract-lottery/src/Raffle.sol
+++ foundry-smart-contract-lottery/src/Raffle.sol
@@ -14,6 +14,9 @@ contract Raffle {
     uint256 private immutable i_entranceFee;
     address payable[] private s_players;
 
+    /* Events */
+    event RaffleEntered(address indexed player);
+
     constructor(uint256 _entranceFee) {
         i_entranceFee = _entranceFee;
     }
@@ -24,6 +27,8 @@ contract Raffle {
             revert NotEnoughEth();
         }
         s_players.push(payable(msg.sender));
+
+        emit RaffleEntered(msg.sender);
     }
 
     function pickWinner() public {

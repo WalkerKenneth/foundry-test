fe0c6fd5ba4e3c5ef162adcaa46c89b01206ee5b
WalkerKenneth <kennethesteban@hotmail.com>
2024-10-30 08:06:07 -0600

    Contract implementation

--- foundry-smart-contract-lottery/src/Raffle.sol
+++ foundry-smart-contract-lottery/src/Raffle.sol
@@ -1,28 +1,47 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.19;
 
+import {VRFConsumerBaseV2Plus} from "@chainlink/contracts/src/v0.8/vrf/dev/VRFConsumerBaseV2Plus.sol";
+import {VRFV2PlusClient} from "@chainlink/contracts/src/v0.8/vrf/dev/libraries/VRFV2PlusClient.sol";
+
 /**
  * @title Raffle
  * @author  Kenneth Walker
  * @notice This contract is a raffle contract that allows users to buy tickets
  * @dev Implements Chainlink VRF to generate random numbers
  */
-contract Raffle {
+contract Raffle is VRFConsumerBaseV2Plus {
     /*Errors */
     error NotEnoughEth();
 
+    uint16 private constant REQUEST_CONFIRMATION = 3;
+    uint32 private constant NUM_WORDS = 1;
+
     uint256 private immutable i_entranceFee;
     uint256 private immutable i_interval;
+    uint256 private immutable i_subscriptionId;
+    uint32 private immutable i_callbackGasLimit;
+    bytes32 private immutable i_keyHash;
     uint256 private s_lastTimeStamp;
     address payable[] private s_players;
 
     /* Events */
     event RaffleEntered(address indexed player);
 
-    constructor(uint256 _entranceFee, uint256 _interval) {
+    constructor(
+        uint256 _entranceFee,
+        uint256 _interval,
+        address vrfCoordinatorV2,
+        bytes32 gasLane,
+        uint256 subscriptionId,
+        uint32 callbackGasLimit
+    ) VRFConsumerBaseV2Plus(vrfCoordinatorV2) {
         i_entranceFee = _entranceFee;
         i_interval = _interval;
         s_lastTimeStamp = block.timestamp;
+        i_keyHash = gasLane;
+        i_subscriptionId = subscriptionId;
+        i_callbackGasLimit = callbackGasLimit;
     }
 
     function enterRaffle() external payable {
@@ -41,7 +60,26 @@ contract Raffle {
         } else {
             s_lastTimeStamp = block.timestamp;
         }
+
+        uint256 requestId = s_vrfCoordinator.requestRandomWords(
+            VRFV2PlusClient.RandomWordsRequest({
+                keyHash: i_keyHash,
+                subId: i_subscriptionId,
+                requestConfirmations: REQUEST_CONFIRMATION,
+                callbackGasLimit: i_callbackGasLimit,
+                numWords: NUM_WORDS,
+                extraArgs: VRFV2PlusClient._argsToBytes(
+                    // Set nativePayment to true to pay for VRF requests with Sepolia ETH instead of LINK
+                    VRFV2PlusClient.ExtraArgsV1({nativePayment: false})
+                )
+            })
+        );
     }
+
+    function fulfillRandomWords(
+        uint256 requestId,
+        uint256[] calldata randomness
+    ) internal override {}
 
     /* Getter Functions */
     function getEntranceFee() public view returns (uint256) {

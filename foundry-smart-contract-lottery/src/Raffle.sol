ac354946714f919e69466d86f925df972c81b9ca
WalkerKenneth <kennethesteban@hotmail.com>
2024-10-29 20:26:55 -0600

    Revert

--- foundry-smart-contract-lottery/src/Raffle.sol
+++ foundry-smart-contract-lottery/src/Raffle.sol
@@ -12,16 +12,20 @@ contract Raffle {
     error NotEnoughEth();
 
     uint256 private immutable i_entranceFee;
+    uint256 private immutable i_interval;
+    uint256 private s_lastTimeStamp;
     address payable[] private s_players;
 
     /* Events */
     event RaffleEntered(address indexed player);
 
-    constructor(uint256 _entranceFee) {
+    constructor(uint256 _entranceFee, uint256 _interval) {
         i_entranceFee = _entranceFee;
+        i_interval = _interval;
+        s_lastTimeStamp = block.timestamp;
     }
 
-    function enterRaffle() public payable {
+    function enterRaffle() external payable {
         // require(msg.value >= i_entranceFee, "Not enough ETH to enter the raffle");
         if (msg.value < i_entranceFee) {
             revert NotEnoughEth();
@@ -31,8 +35,12 @@ contract Raffle {
         emit RaffleEntered(msg.sender);
     }
 
-    function pickWinner() public {
-        // Implement the logic to pick a winner
+    function pickWinner() external {
+        if (block.timestamp - s_lastTimeStamp > i_interval) {
+            revert();
+        } else {
+            s_lastTimeStamp = block.timestamp;
+        }
     }
 
     /* Getter Functions */
